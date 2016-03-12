896a87301d8b5d95ffd380826e0f67e64d4727a0
kylescott <ky_scotty4@Hotmail.com>
2016-03-11 21:17:56 -0600

    first real push

---
+++ Game Jam 2016/Assets/Scripts/MoveCogWheel.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveCogWheel : MonoBehaviour {
+
+    Transform cogTransform;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        cogTransform = GetComponent<Transform>();
+        cogTransform.Rotate(0, 0, 1);
+	}
+}
---
+++ Game Jam 2016/Assets/Scripts/shoot.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class shoot : MonoBehaviour {
+
+    bool hasClicked = false;
+
+	// Use this for initialization
+	void Start () {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButtonDown(0))
+        {
+                hasClicked = true;
+                Vector3 position = this.transform.position;
+                position.y += 0.25f;
+                this.transform.position = position;
+        }
+        if (hasClicked)
+        {
+            Vector3 position = this.transform.position;
+            position.y += 0.25f;
+            this.transform.position = position;
+        }
+	}
+
+    void OnCollisionEnter(Collision2D col)
+    {
+        GameObject arrow = GameObject.FindGameObjectWithTag("arrow_blue");
+        GameObject cogWheel = GameObject.FindGameObjectWithTag("cogWheel");
+        if (col.gameObject.tag == "cogWheel")
+        {
+            Debug.Log("hi");
+            // attach a to b
+            arrow.transform.parent = cogWheel.transform;
+            // make sure its exactly on it
+            arrow.transform.localPosition = Vector3.zero;
+            arrow.transform.localRotation = Quaternion.identity;
+            
+        }
+    }
+}
